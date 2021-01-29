14dfd5bffb512c931b0fd06d2d4e34577f7f91df
Rodrigo Lopez <rlopez@montevideolabs.com>
2021-01-29 12:41:18 -0300

    Root fix

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ const path = require('path');
 const serveStatic = require('serve-static');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(serveStatic(path.join(__dirname, 'public')));
+app.use(serveStatic(path.join(__dirname, '/')));
 app.listen(port,  function () {
     console.log('listening on port:', port);
 });
